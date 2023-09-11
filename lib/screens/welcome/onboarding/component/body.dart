95d91779e64ff672c7b0ceb1df41617b3bcea15e
Deepak <deepak20c@gmail.com>
2023-09-11 14:53:09 -0400

    update routes

--- lib/screens/welcome/onboarding/component/body.dart
+++ lib/screens/welcome/onboarding/component/body.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:getten/screens/auth/signin/signin_screen.dart';
 import 'package:getten/screens/welcome/onboarding/component/cicrcle_progress_bar.dart';
 import 'package:getten/screens/welcome/onboarding/component/onboarding_widget.dart';
 import 'package:getten/state/onboarding_state.dart';
 import 'package:getten/utils/assets/colors/colors.dart';
 import 'package:getten/utils/models/onbarding/data.dart';
 import 'package:getten/utils/responsive/dimension.dart';
+import 'package:getten/utils/routes/routes_name.dart';
 
 class BodyOnboardingScreen extends StatelessWidget {
   final onboardingController = Get.put(OnboardingController());
@@ -24,7 +24,9 @@ class BodyOnboardingScreen extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          SizedBox(height: dimensions.getScreenHeight * 0.02),
+          SizedBox(
+            height: dimensions.getScreenHeight * 0.02,
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -45,12 +47,7 @@ class BodyOnboardingScreen extends StatelessWidget {
                   : const SizedBox(),
               TextButton(
                 onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const SignInScreen(),
-                    ), //MaterialPageRoute
-                  );
+                  Get.offNamed(RoutesName.signInScreen);
                 },
                 child: Text(
                   'Skip',
@@ -63,7 +60,7 @@ class BodyOnboardingScreen extends StatelessWidget {
             ],
           ),
           SizedBox(
-            height: dimensions.getScreenHeight * 0.05,
+            height: dimensions.getScreenHeight * 0.1,
           ),
           Container(
             padding: EdgeInsets.all(dimensions.getScreenWidth * 0.02),
@@ -125,13 +122,7 @@ class BodyOnboardingScreen extends StatelessWidget {
                                   curve: Curves.ease,
                                 )
                               : () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) =>
-                                          const SignInScreen(),
-                                    ), //MaterialPageRoute
-                                  );
+                                  Get.offNamed(RoutesName.signInScreen);
                                 };
                         },
                         icon: const Icon(
